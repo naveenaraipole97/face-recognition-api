7e900b6ed169f7d92c09cdcdb6f69e4ed8a0e2ec
Naveena Raipole <144768728+naveenaraipole97@users.noreply.github.com>
2024-05-07 18:24:00 -0700

    Delete aws-cli-commands directory

--- aws-cli-commands/ami.sh
+++
@@ -1,5 +0,0 @@
-rm -rf /home/ec2-user/app
-mkdir -p /home/ec2-user/app/face_recognition_part2
-cd /tmp/
-git clone https://github.com/naveenaraipole97/face_recognition_part2.git
-cp -r face_recognition_part2/model /home/ec2-user/app/face_recognition_part2/
--- aws-cli-commands/userdata.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-su ec2-user -c 'aws s3 cp s3://file-processing-app . --recursive &&  cd file_processing_app2 && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash && source ~/.bashrc && nvm install --lts && npm i && npm run start'
-
